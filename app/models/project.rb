df034f23e090d6d7044752076e67316d4b50ab1a
Alex Roberts <alex@redprocess.com>
2011-01-14 15:30:33 -0800

    Fixed nested attributes definition to properly allow adding/destroying images

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,13 @@ class Project < ActiveRecord::Base
   before_save :parameterize_name
   
   belongs_to :category
-  has_many :images
+  has_many :images, :dependent => :destroy
   
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :category_id
   
-  accepts_nested_attributes_for :images, :reject_if => lambda { |a| a[:image].blank? }
+  accepts_nested_attributes_for :images, :reject_if => proc {|a| a['title'].blank? }, :allow_destroy => :true
   
   def to_param
     slug
