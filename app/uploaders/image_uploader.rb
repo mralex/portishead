c026b7df4943a15ce20787a46f1aeccf448eeb89
Alex Roberts <alex@redprocess.com>
2011-01-14 17:25:28 -0800

    Added hero image attachment size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,6 +17,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [300, 200]
   end
 
+  version :hero do 
+    process :resize_to_fill => [800, 300]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
