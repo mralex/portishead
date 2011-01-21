63296fabedac5a3d85dd3de3e01164fd95fbeb1f
Alex Roberts <alex@redprocess.com>
2011-01-21 15:33:18 -0800

    Revised ordering of animations and page loading; Set the height of the content box container before showing the new content so we don't get any weird resizing and clipping issues on content load.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,14 +1,23 @@
 $(function() {
 	
 	$.history.init(function(url) {
-		if (url) {
-			$('html, body').animate({scrollTop:0}, 'slow', 'swing');
-
-			$("#content_box").slideUp('fast');
-			$.getScript(url + '.js');
-			$("#content_box").slideDown('fast');
+		if (url && (url.length > 1)) {
+			$('html, body').animate({scrollTop:0}, 400, 'swing');
+		
+			$("#content_box").hide('slide', {direction: 'left'}, 'fast', function(e) {
+				$.getScript(url + '.js', function(e) {
+					// Animate height change?
+					$("#index_hero").css("height", $("#content_box").height());
+					
+					$("#content_box").show('slide', {direction: 'right'}, 'fast');
+				});
+			});
+			
 		} else {
-			$.getScript("/projects/heroes.js");
+			$("#index_hero").css("height", 'inherit');
+			$.getScript("/projects/heroes.js", function(e) {
+				$("#index_hero").css("height", $("#content_box").height());
+			});
 		}
 	},{ unescape: "/" });
 
@@ -17,6 +26,11 @@ $(function() {
 		url = url.replace(/^.*#/, '');
 		$.history.load(url);
 		
+		e.preventDefault();
+	});
+	
+	$('#header h1 a').click(function(e) {
+		$.history.load('');
 		e.preventDefault();
 	});
 
