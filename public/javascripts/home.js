e066911801980c69a1ce8c56a0af4f6ccd1adb4e
Alex Roberts <alex@redprocess.com>
2011-01-21 14:21:38 -0800

    First stab at using jquery.history to enable hash urls. It, just, works. (All links currently open the hero box.)

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,23 @@
 $(function() {
 	
-	$.getScript("/projects/heroes.js");
-	
+	$.history.init(function(url) {
+		if (url) {
+			$('html, body').animate({scrollTop:0});
 
+			$("#content_box").slideUp('fast');
+		//	$.getScript(url);
+			$.getScript("/projects/heroes.js");
+			$("#content_box").slideDown('fast');
+		} else {
+			$.getScript("/projects/heroes.js");
+		}
+	},{ unescape: "/" });
+
+	$('a.project_link').live('click', function(e) {
+		var url = $(this).attr('href');
+		url = url.replace(/^.*#/, '');
+		$.history.load(url);
+		return false;
+	});
 
 });
