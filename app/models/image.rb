82229ebc25f4790733737b8046d3f3e5f6b99b78
Alex Roberts <alex@redprocess.com>
2011-02-02 19:05:22 -0800

    Make sure specifically sized uploads don't get modified by ImageMagick
    940x300 banners and 300x200 project images can now be uploaded without getting resaved with quality and sharpening issues.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,7 @@ class Image < ActiveRecord::Base
   acts_as_list :scope => :project
   mount_uploader :image, ImageUploader
   
+  before_save :ensure_uploaded_sizes
   before_save :update_dimensions
   
   validates_presence_of :title
@@ -29,5 +30,26 @@ class Image < ActiveRecord::Base
     display = Magick::Image.read(Rails.root.to_s + "/public" + self.image.display.url).first
     self.display_width = display.columns
     self.display_height = display.rows
+  end
+  
+  def ensure_uploaded_sizes
+    public_path = Rails.root.to_s + "/public"
+    image = Magick::Image.read(public_path + self.image.url).first
+    
+    w = image.columns
+    h = image.rows
+    
+    if (w == 940 && h == 300) 
+      FileUtils.cp(public_path + self.image.url, public_path + self.image.hero.url)
+    end
+    
+    if (w == 300 && h == 200) 
+      FileUtils.cp(public_path + self.image.url, public_path + self.image.small.url)
+    end
+    
+    # FIXME: This might be incorrect
+    if (w <= 700 && h <= 700) 
+      FileUtils.cp(public_path + self.image.url, public_path + self.image.display.url)
+    end
   end
 end
