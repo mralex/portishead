cb7a1bf02449494ea18bfd75af27021067426be5
Alex Roberts <alex@redprocess.com>
2011-02-03 23:34:28 -0800

    Rearranged project#show scripts to exist mainly in the project.js as an object function. The JS version and regular version now instantiate a new ProjectCarousel object.

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -1,146 +1,98 @@
-function updateNavHeight(slide) {
-	$("#image_view_nav a").css("height", slide.find("img").height());
-}
+function ProjectCarousel() {
 
-$(window).load(function() {
-	
+	function updateNavHeight(slide) {
+		$("#image_view_nav a").css("height", slide.find("img").height());
+	}
+
+
 	var firstSlide = $("#image_view li:first");
 	var lastSlide = $("#image_view li:last");
 
-	$("#image_view_nav a.next").click(function(e) {
-		var current = $("#image_view li.visible");
-		var next = current.next();
-	
-		$("#image_view li").removeClass("visible");
-	
-		if (next.length == 0) {
-			// loop back
-			next = firstSlide;
+	if ($("#image_view li").length > 1) {
+		// Project Images
+		// 
+		// Slide carousel
+
+		var carouselPaused = false;
+		var currentSlide = null;
+		var slideTimeout = null;
+		
+		function updateSlide(slide) {
+			currentSlide = slide;
+			
+			updateNavHeight($(currentSlide));
+			
+			nextSlide(slide);
 		}
-	
-		next.addClass("visible");
-		$("#image_view li").fadeOut('fast', function(e) {
-			updateNavHeight(next);
-		});
-		next.fadeIn('fast');
-	
-		e.preventDefault();
-	});
+		
+		function nextSlide(slide, delay) {
+			var stdDelayTime = 5000;
+			var fadeTime = 500;
 
-	$("#image_view_nav a.prev").click(function(e) {
-		var current = $("#image_view li.visible");
-		var previous = current.prev();
-	
-		$("#image_view li").removeClass("visible");
-	
-		if (previous.length == 0) {
-			// loop back
-			previous = lastSlide;
+			if (carouselPaused) return;
+
+			slideTimeout = window.setTimeout(function(e) {
+				$(slide).fadeOut(fadeTime);
+				if ($(slide).next().length == 0) {
+					$(slide).parent().find("li").first().fadeIn(fadeTime, function() { updateSlide(this); });
+				} else {
+					$(slide).next().fadeIn(fadeTime, function() { updateSlide(this); });
+				}
+			}, delay ? delay : stdDelayTime);
 		}
-	
-		previous.addClass("visible");
-		$("#image_view li").fadeOut('fast', function(e) {
-			updateNavHeight(previous);
+
+		$("#image_view li").hide();
+		$("#image_view li").first().show(0, function(e) {
+			currentSlide = this;
+			nextSlide(this);
+			updateNavHeight($(this));
+			$("#image_view_nav a").css('opacity', '0');
 		});
-		previous.fadeIn('fast');
-	
-		e.preventDefault();
-	});
 
+		$("#image_view ul, #image_view_nav a").hover(function() {
+			window.clearTimeout(slideTimeout);
+			carouselPaused = true;
+		}, function() {
+			carouselPaused = false;
+			nextSlide(currentSlide, 1000);
+		});
+		
+		$("#image_view_nav a.next").click(function(e) {
+			var next = $(currentSlide).next();
 
-	var firstSlide = $("#image_view li:first");
+			if (next.length == 0) {
+				// loop back
+				next = firstSlide;
+			}
 
-	$("#image_view li").hide();
-	firstSlide.show();
-	firstSlide.addClass("visible");
+			$("#image_view li").fadeOut('fast', function(e) {
+				updateNavHeight(next);
+			});
+			next.fadeIn('fast');
+			
+			currentSlide = next[0];
 
-	var tallest = 0;
-	$("#image_view li").each(function(e) {
-		if ($(this).height() > tallest) tallest = $(this).height();
-	});
-	
-	if (tallest > 0) {
-		// var projectHeight = $("#project").height() - $("#image_view").height() + tallest;
-		// 	
-		// 	if (projectHeight != $("#project").height()) {
-		// 		$("#project").animate({height: projectHeight}, {duration: 200});
-		// 	}
-		$("#image_view").height(tallest);
+			e.preventDefault();
+		});
+
+		$("#image_view_nav a.prev").click(function(e) {
+			var previous = $(currentSlide).prev();
+
+			if (previous.length == 0) {
+				// loop back
+				previous = lastSlide;
+			}
+
+			$("#image_view li").fadeOut('fast', function(e) {
+				updateNavHeight(previous);
+			});
+			previous.fadeIn('fast');
+			
+			currentSlide = previous[0];
+			
+			e.preventDefault();
+		});
 	}
-
-	updateNavHeight(firstSlide);
-	$("#image_view_nav a").delay(600).animate({opacity: 0}, {duration: 400});
-});
-
-// $(function() {
-// 	$("#image_view li").hide();
-// 	$("#image_view li:first").show();
-// 	$("#image_view li:first").addClass("visible");
-// 
-// 	$("#image_view_nav a.next").click(function(e) {
-// 		var current = $("#image_view li.visible");
-// 		var next = current.next();
-// 
-// 		$("#image_view li").removeClass("visible");
-// 
-// 		if (next.length == 0) {
-// 			// loop back
-// 			next = $("#image_view li:first");
-// 		}
-// 
-// 		next.addClass("visible");
-// 		$("#image_view li").fadeOut('fast');
-// 		next.fadeIn('fast');
-// 
-// 		e.preventDefault();
-// 	});
-// 
-// 	$("#image_view_nav a.prev").click(function(e) {
-// 		var current = $("#image_view li.visible");
-// 		var previous = current.prev();
-// 
-// 		$("#image_view li").removeClass("visible");
-// 
-// 		if (previous.length == 0) {
-// 			// loop back
-// 			previous = $("#image_view li:last");
-// 		}
-// 
-// 		previous.addClass("visible");
-// 		$("#image_view li").fadeOut('fast');
-// 		previous.fadeIn('fast');
-// 
-// 		e.preventDefault();
-// 	});
-// 	
-// // var currentId;
-// // 
-// // var id = currentId = $("#images li").first().attr("data-id");
-// // $.getJSON(document.href + "/images/" + id, function(data) {
-// // 	$("#image_view").append('<li class="image_'+ data.image.id +'"><img src="' + data.image.image.url +'" /></li>')
-// // });
-// // 
-// // $("#images li a").live("click", function(e) {
-// // 	var id = $(this).parent().attr("data-id");
-// // 	
-// // 	if (id == currentId) {
-// // 		e.preventDefault();
-// // 		return;
-// // 	}
-// // 	
-// // 	if ($("#image_view").has(".image_" + id).length == 0) {
-// // 		$.getJSON(document.href + "/images/" + id, function(data) {
-// // 			$("#image_view li").fadeOut('slow');
-// // 			$("#image_view").append('<li class="image_'+ data.image.id +'"><img src="' + data.image.image.url +'" /></li>').fadeIn('slow');
-// // 		});
-// // 	} else {
-// // 		$("#image_view li").fadeOut('fast');
-// // 		$("#image_view li.image_" + id).fadeIn('fast');
-// // 	}
-// // 	
-// // 	currentId = id;
-// // 	
-// // 	e.preventDefault();
-// // });
-// 
-// });
+	// end of carousel
+	
+}
