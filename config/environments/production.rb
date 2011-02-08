06eeaa02718362c58a739ab32b0f9e226d84021e
Alex Roberts <alex@redprocess.com>
2011-02-07 23:38:42 -0800

    Fixed issue in production where OpenSSL would complain about hostname not matching server certificate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Portishead::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # Turn off auto TLS for e-mail
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 end
