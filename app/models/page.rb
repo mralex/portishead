bdc865cf5f6ed5e152b707905b0fb29b133a636f
Alex Roberts <alex@redprocess.com>
2011-02-08 10:26:13 -0800

    parameterize page slugs before saving

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,15 @@
 class Page < ActiveRecord::Base
+  before_save :parameterize_slug
+  
   validates :slug, :presence => true, :uniqueness => true
   validates :slug, :format => { :with => /^[-A-Za-z0-9\d_]+$/ }
   
   def to_param
     slug.parameterize
+  end
+  
+  private
+  def parameterize_slug
+    self.slug = self.slug.parameterize
   end
 end
