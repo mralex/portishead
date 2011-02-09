374c082cff676c19e71a397a254bfa88341d97d1
Alex Roberts <alex@redprocess.com>
2011-02-08 21:29:13 -0800

    Added Javascript contact form validation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,27 @@ if ($("#flash_notice").html()) {
 	$("#flash_notice").delay(3000).hide('slide', 'fast');
 }
 
+
+// Contact form validation
+$("#contact_form form").submit(function(e) {
+	var error = false;
+	
+	$("#contact_form span").html("");
+	
+	if ($("#email").val() === "") {
+		$("#email").siblings(".error").html("Email address required.");
+		error = true;
+	}
+	
+	if ($("#message").val() === "") {
+		$("#message").siblings(".error").html("Message required.");
+		error = true;
+	}
+	
+	if (error) {
+		e.preventDefault();
+		return false;
+	}
+	
+});
 });
